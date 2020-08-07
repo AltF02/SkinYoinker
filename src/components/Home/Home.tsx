8c89958794f0c05c05d15e53518a36dc5bf56dca
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-08-07 05:44:58 +0200

    functioning prototype

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import './Home.scss'
-import { Input } from "semantic-ui-react";
+// import { Input } from "semantic-ui-react";
+import getUserSkinUrl  from '../../services/api/Mojang'
 
 class Home extends React.Component<any, any> {
-    input_text: { value: string };
     constructor(props: any) {
         super(props);
-        this.input_text = {value: ''};
+        this.state = {value: '', url: ''};
 
-        this.handleChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.searchSkin = this.searchSkin.bind(this)
     }
 
     handleChange (event: any) {
-        alert(event.target.value)
         this.setState({value: event.target.value})
+    }
+
+    searchSkin () {
+        console.log(this.state.value)
+        getUserSkinUrl(this.state.value).then(response => this.setState({url: response}))
     }
 
     render() {
         return (
             <div className='homeOuterContainer'>
-                <h1>Hello</h1>
-                <Input type='text' value={this.input_text} onChange={this.handleChange}/>
+                <h1>{this.state.value}</h1>
+                {this.state.url ? <a href={this.state.url}>Link</a>: null}
+                <img src={this.state.url} alt=''/>
+                <input type='text' value={this.state.value} onChange={this.handleChange}/>
+                <button onClick={this.searchSkin}>Get link</button>
             </div>
         )
     }
--- src/services/api/Mojang.ts
+++ src/services/api/Mojang.ts
@@ -1,12 +1,45 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-function getUserId(name: string) {
-    return axios.get('https://api.mojang.com/users/profiles/minecraft/' + name)
-        .then(response => {
-            return response.data
-    })
+type getUser = {
+    name: string,
+    id: string
 }
 
-export default {
-    getUserId
+type userProfile = {
+    id: string,
+    name: string,
+    properties: [{
+        name: string,
+        value: string
+    }]
 }
+
+type userProperties = {
+    timestamp : Number,
+    profileId : string,
+    profileName : string,
+    textures : {
+        SKIN : {
+            url : string
+        }
+    }
+}
+
+
+async function getUserId(name: string): Promise<AxiosResponse<getUser>> {
+    return await axios.get('https://api.mojang.com/users/profiles/minecraft/' + name)
+}
+
+async function getUserSkinUrl(name: string): Promise<string> {
+    let user = await getUserId(name);
+    let user_uid = user.data['id'];
+
+    let user_profile: AxiosResponse<userProfile> = await axios.get('https://sessionserver.mojang.com/session/minecraft/profile/' + user_uid)
+    console.log(user_profile)
+    let properties: userProperties = JSON.parse(atob(user_profile.data['properties'][0]['value']))
+
+    return properties['textures']['SKIN']['url']
+}
+
+export default getUserSkinUrl
+
